341e638a1168a48ee63104a675fb0a2e445d80f0
Sulton <sazamatov1207@gmail.com>
2025-06-20 01:42:05 +0500

    Добавил app.py, main.py

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e388870a7c240a4e21bdfbef6ac4539a47d4c4be15f2d35c86ad1c029711a29a
+size 269
---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d882e1d3ddf37b6c136c9b42be5017eb5a19cb2b945daf93d1bf228ef531d09
+size 7370
