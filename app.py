06fb686eef88b744bab55ef96904c19620e21548
Sulton <sazamatov1207@gmail.com>
2025-06-20 01:30:12 +0500

    Удалил файлы

--- app.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:73feebc71312e3158950dfc7d4984bf61dc091d3b193105a2b6dcdaf7abd222f
-size 271
--- main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0fd26a0abe7e5b091f54a4d2eab8d5a3ff2dc2b6647eea159e8f0d39cfd25997
-size 8450
